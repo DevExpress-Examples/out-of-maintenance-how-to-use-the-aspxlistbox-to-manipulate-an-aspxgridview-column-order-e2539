2cbbab7701224654716df9e9a874b8c6527db36d
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ Readme.md
@@ -0,0 +1,8 @@
+# How to use the ASPxListBox to manipulate an ASPxGridView column order
+
+
+<p>This example demonstrates how to move a grid's column on a click of a button. Visually, you can see the modification in the ASPxListBox.</p>
+
+<br/>
+
+
---
+++ VB/WebSite/Default.aspx.vb
@@ -0,0 +1,36 @@
+﻿Imports Microsoft.VisualBasic
+Imports System
+Imports System.Collections.Generic
+Imports System.Web
+Imports System.Web.UI
+Imports System.Web.UI.WebControls
+Imports DevExpress.Web.ASPxGridView
+Imports DevExpress.Web.ASPxEditors
+Imports DevExpress.Web.ASPxClasses
+
+Partial Public Class _Default
+	Inherits System.Web.UI.Page
+	Protected Sub Page_Load(ByVal sender As Object, ByVal e As EventArgs)
+	End Sub
+	Protected Sub ASPxListBox1_Init(ByVal sender As Object, ByVal e As EventArgs)
+		PopulateListBox()
+	End Sub
+	Protected Sub ASPxListBox1_Callback(ByVal sender As Object, ByVal e As CallbackEventArgsBase)
+		PopulateListBox()
+	End Sub
+	Private Sub PopulateListBox()
+		ASPxListBox1.Items.Clear()
+		For i As Integer = 0 To ASPxGridView1.VisibleColumns.Count - 1
+			ASPxListBox1.Items.Add((TryCast(ASPxGridView1.VisibleColumns(i), GridViewDataColumn)).FieldName, i)
+		Next i
+	End Sub
+	Protected Sub ASPxGridView1_CustomCallback(ByVal sender As Object, ByVal e As ASPxGridViewCustomCallbackEventArgs)
+		Select Case e.Parameters
+			Case "UP"
+				ASPxGridView1.Columns(ASPxListBox1.SelectedItem.Text).VisibleIndex -= 1
+			Case "DOWN"
+				ASPxGridView1.Columns(ASPxListBox1.SelectedItem.Text).VisibleIndex += 1
+		End Select
+	End Sub
+
+End Class
